3b0c45ee5fb15919cfbf76a7b402f16b3ee53491
student <student@ip-172-31-59-74.us-west-2.compute.internal>
2021-02-02 14:29:05 -0800

    a

--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -14,12 +14,20 @@ public class SleepyHead {
 		// Write code to ask the user what day it is.
         String day = JOptionPane.showInputDialog(null,"What day is it?");		
 		// Set the boolean isWeekend based on the value they enter
-		
-    
+		if(day.equals("Saturday")||day.equals("Sunday")) {
+			isWeekend = true;
+		}
+		else {
+			isWeekend = false;
+		}
 		// If it is the weekend, tell the user they get to sleep in.
-		
+		if(isWeekend = true) {
+			JOptionPane.showMessageDialog(null,"You get to sleep in!");
+		}
 		// If it is not the weekend, tell them to get out of bed and go to school!
-		
+		if(isWeekend = false) {
+			JOptionPane.showMessageDialog(null,"Get out of bed and go to school!");
+		}
 		
 		/************************   STAR STUDENT  **************************/
 
