a437dd4ccc38dc0397b310724ec35ef1a29c4d17
student <student@ip-172-31-62-125.us-west-2.compute.internal>
2021-02-02 18:55:13 -0800

    a

--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -10,7 +10,7 @@ public class SleepyHead {
 		// boolean variables can only hold one of two values - true or false
 
 		/************************  SLEEPY HEAD  **************************/
-		boolean isWeekend;
+	boolean isWeekend;
 		// Write code to ask the user what day it is.
         String day = JOptionPane.showInputDialog(null,"What day is it?");		
 		// Set the boolean isWeekend based on the value they enter
@@ -21,11 +21,11 @@ public class SleepyHead {
 			isWeekend = false;
 		}
 		// If it is the weekend, tell the user they get to sleep in.
-		if(isWeekend = true) {
+		if(isWeekend == true) {
 			JOptionPane.showMessageDialog(null,"You get to sleep in!");
 		}
 		// If it is not the weekend, tell them to get out of bed and go to school!
-		if(isWeekend = false) {
+		if(isWeekend == false) {
 			JOptionPane.showMessageDialog(null,"Get out of bed and go to school!");
 		}
 		
@@ -34,14 +34,24 @@ public class SleepyHead {
 		boolean passedExam;
 		// Write code to ask the user what percentage they scored in their last exam
 
-		
+		String code = JOptionPane.showInputDialog("What percentage did you score on you last exam?");
 		// If they scored more than 70, they passed the exam.
 		// Set the boolean passedExam based on their score.
-		
+		int Code = Integer.parseInt(code);
+	if(Code > 70) {
+		passedExam = true;
+	}
+	else {
+		passedExam = false;
+	}
 		// If the user passed the exam, congratulate them
-		
+		if(passedExam == true) {
+			JOptionPane.showMessageDialog(null,"Congratulations! You did a good job.");
+		}
 		// otherwise, wish them better luck next time.
-		
+		if(passedExam == false) {
+			JOptionPane.showMessageDialog(null, "Better luck next time.");
+		}
 		
 		/************************  GAME OVER  **************************/
 
@@ -57,23 +67,40 @@ public class SleepyHead {
 
 		}
 		// Tell the user "game is over" 
-		
+		JOptionPane.showMessageDialog(null, "Game is over");
 		
 		/***********************   RED SQUARE  ********************/
 		
 		boolean isRed;
 		// Ask the user what color to draw with. Based on their answer, set the isRed variable
-		
-		
+		String color = JOptionPane.showInputDialog("what color do you draw with?");
+		if(color.equals("red")) {
+			isRed = true;
+		}
+		else {
+			isRed = false;
+		}
+	
 		boolean isSquare;		
 		// Now ask the user what shape to draw. Based on their answer, set the isSquare variable
+		String shape = JOptionPane.showInputDialog("What shape do you want to draw?");
+		if(shape.equals("square")) {
+			isSquare = true;
+		}
 		
-		
+		else {
+			isSquare = false;
+		}
 		// Now you MUST use the && operator to join the booleans for this code. 
 		// Ask your teacher if you are not sure how
 		// Use an if statement to ONLY draw a red square when it has been requested (use the method below)
 		// otherwise, tell the user you don't know how to draw that shape
-		
+		if(isRed == true && isSquare == true) {
+			drawRedSquare();
+		}	
+		else {
+			JOptionPane.showMessageDialog(null, "I don't know how to draw that shape");
+		}
 
 	}
 
--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,25 +5,29 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+String message = "hey";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
 
-		
+		System.out.println(message.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(message.length());
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for(int Char = 0; Char < message.length(); Char+=1) {
+	System.out.println(message.charAt(Char));
+}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		for(int position = 0; position < message.length();position +=1) {
+			System.out.println(message.charAt(position) +" is at index "+position);
+		}
 	}
 }
 
